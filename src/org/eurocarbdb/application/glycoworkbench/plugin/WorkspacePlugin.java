852f7dbf9b567ebb8ec9786957b75f8597f3b342
David Damerell (david <David Damerell (david@nixbioinf.org)>
2010-10-20 13:18:09 +0100

    LAF hot swap prototype

--- src/org/eurocarbdb/application/glycoworkbench/plugin/WorkspacePlugin.java
+++ src/org/eurocarbdb/application/glycoworkbench/plugin/WorkspacePlugin.java
@@ -1,25 +1,25 @@
 /*
-*   EuroCarbDB, a framework for carbohydrate bioinformatics
-*
-*   Copyright (c) 2006-2009, Eurocarb project, or third-party contributors as
-*   indicated by the @author tags or express copyright attribution
-*   statements applied by the authors.  
-*
-*   This copyrighted material is made available to anyone wishing to use, modify,
-*   copy, or redistribute it subject to the terms and conditions of the GNU
-*   Lesser General Public License, as published by the Free Software Foundation.
-*   A copy of this license accompanies this distribution in the file LICENSE.txt.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-*   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
-*   for more details.
-*
-*   Last commit: $Rev$ by $Author$ on $Date::             $  
-*/
+ *   EuroCarbDB, a framework for carbohydrate bioinformatics
+ *
+ *   Copyright (c) 2006-2009, Eurocarb project, or third-party contributors as
+ *   indicated by the @author tags or express copyright attribution
+ *   statements applied by the authors.  
+ *
+ *   This copyrighted material is made available to anyone wishing to use, modify,
+ *   copy, or redistribute it subject to the terms and conditions of the GNU
+ *   Lesser General Public License, as published by the Free Software Foundation.
+ *   A copy of this license accompanies this distribution in the file LICENSE.txt.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ *   for more details.
+ *
+ *   Last commit: $Rev$ by $Author$ on $Date::             $  
+ */
 /**
-   @author Alessio Ceroni (a.ceroni@imperial.ac.uk)
-*/
+ @author Alessio Ceroni (a.ceroni@imperial.ac.uk)
+ */
 
 package org.eurocarbdb.application.glycoworkbench.plugin;
 
@@ -33,122 +33,122 @@ import java.util.*;
 
 import javax.swing.ImageIcon;
 
+public class WorkspacePlugin implements Plugin {
 
-public class WorkspacePlugin implements Plugin{
+	PluginManager theManager = null;
+	GlycoWorkbench theApplication = null;
+	GlycanWorkspace theWorkspace = null;
 
-    PluginManager theManager = null;
-    GlycoWorkbench theApplication = null;
-    GlycanWorkspace theWorkspace = null;
+	WorkspacePanel theWorkspacePanel = null;
 
-    WorkspacePanel theWorkspacePanel = null;    
-    
-    public WorkspacePlugin(GlycoWorkbench application) {
-    	this.theApplication=application;
-    theWorkspacePanel = new WorkspacePanel(application);
-    
-    }
+	public WorkspacePlugin(GlycoWorkbench application) {
+		this.theApplication = application;
+		theWorkspacePanel = new WorkspacePanel(application);
 
-    public void init() {
-    }
+	}
 
-    public void exit() {
-    }
+	public void init() {
+	}
 
-    public String getName() {
-    return "Workspace";
-    }
+	public void exit() {
+	}
 
-    public int getMnemonic() {
-    return java.awt.event.KeyEvent.VK_W;
-    }
-    
-    public ImageIcon getIcon() {
-    return ThemeManager.getEmptyIcon(ICON_SIZE.TINY);
-    }
+	public String getName() {
+		return "Workspace";
+	}
 
-    public int getViewPosition(String view) {
-    return PluginManager.VIEW_LEFT;
-    }
+	public int getMnemonic() {
+		return java.awt.event.KeyEvent.VK_W;
+	}
 
-    public java.awt.Component getLeftComponent() {
-    return theWorkspacePanel;
-    }
+	public ImageIcon getIcon() {
+		return ThemeManager.getEmptyIcon(ICON_SIZE.TINY);
+	}
 
-    public java.awt.Component getRightComponent() {
-    return null;
-    }
+	public int getViewPosition(String view) {
+		return PluginManager.VIEW_LEFT;
+	}
 
-    public java.awt.Component getBottomComponent() {
-    return null;
-    }
- 
-    public Collection<String> getViews() {
-    return Collections.singleton("Workspace");
-    }
+	public java.awt.Component getLeftComponent() {
+		return theWorkspacePanel;
+	}
 
-    public Collection<GlycanAction> getActions() {
-    return new Vector<GlycanAction>();
-    }
+	public java.awt.Component getRightComponent() {
+		return null;
+	}
 
-    public Collection<GlycanAction> getToolbarActions() {
-    return new Vector<GlycanAction>();
-    }
+	public java.awt.Component getBottomComponent() {
+		return null;
+	}
 
-    public Collection<GlycanAction> getObjectActions(Object prototype, java.awt.event.ActionListener al) {
-    Vector<GlycanAction> actions = new Vector<GlycanAction>();
-    return actions;
-    }
+	public Collection<String> getViews() {
+		return Collections.singleton("Workspace");
+	}
 
-    public void setManager(PluginManager manager) {
-    theManager = manager;
-    }
+	public Collection<GlycanAction> getActions() {
+		return new Vector<GlycanAction>();
+	}
 
-    public void setApplication(GlycoWorkbench application) {
-    theApplication = application;
-    theWorkspacePanel.setApplication(application);
-    }
-  
-    public void setWorkspace(GlycanWorkspace workspace) {
-    theWorkspace = workspace;
-    theWorkspacePanel.setWorkspace(workspace);
-    }
-   
-    public  PluginManager getManager() {
-    return theManager;
-    }
+	public Collection<GlycanAction> getToolbarActions() {
+		return new Vector<GlycanAction>();
+	}
 
-    public  GlycoWorkbench getApplication() {
-    return theApplication;
-    }
+	public Collection<GlycanAction> getObjectActions(Object prototype,
+			java.awt.event.ActionListener al) {
+		Vector<GlycanAction> actions = new Vector<GlycanAction>();
+		return actions;
+	}
 
-    public  GlycanWorkspace getWorkspace() {
-    return theWorkspace;
-    }  
+	public void setManager(PluginManager manager) {
+		theManager = manager;
+	}
 
-    public void show(String view) throws Exception {
-    if( !view.equals("Workspace") )
-        throw new Exception("Invalid view: " + view);        
-    }
+	public void setApplication(GlycoWorkbench application) {
+		theApplication = application;
+		theWorkspacePanel.setApplication(application);
+	}
 
-    public boolean runAction(String action) throws Exception {
-    throw new Exception("Invalid action: " + action);
-    }
+	public void setWorkspace(GlycanWorkspace workspace) {
+		theWorkspace = workspace;
+		theWorkspacePanel.setWorkspace(workspace);
+	}
 
-    public boolean runAction(String action, Object param) throws Exception {
-    throw new Exception("Invalid action: " + action);
-    }
+	public PluginManager getManager() {
+		return theManager;
+	}
 
-    public void updateViews() {
-    theWorkspacePanel.updateView();
-    }
+	public GlycoWorkbench getApplication() {
+		return theApplication;
+	}
 
-    public void updateMasses() {
-    }
-    
-    @Override
+	public GlycanWorkspace getWorkspace() {
+		return theWorkspace;
+	}
+
+	public void show(String view) throws Exception {
+		if (!view.equals("Workspace"))
+			throw new Exception("Invalid view: " + view);
+	}
+
+	public boolean runAction(String action) throws Exception {
+		throw new Exception("Invalid action: " + action);
+	}
+
+	public boolean runAction(String action, Object param) throws Exception {
+		throw new Exception("Invalid action: " + action);
+	}
+
+	public void updateViews() {
+		theWorkspacePanel.updateView();
+	}
+
+	public void updateMasses() {
+	}
+
+	@Override
 	public void completeSetup() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
