484648092957cac06d74c20bd541131d49608023
David Damerell <David Damerell@localhost>
2011-09-09 15:30:23 +0100

    [Unstable branch merge]New manual|Moved configuration files|More WGGDS improvements|Iterative charge state annotation

---
+++ src/org/eurocarbdb/application/glycoworkbench/launchers/MacWrapper64.java
@@ -0,0 +1,8 @@
+package org.eurocarbdb.application.glycoworkbench.launchers;
+
+public class MacWrapper64 {
+	public static void main(String args[]){
+		MacWrapper macWrapper=new MacWrapper("64");
+		macWrapper.run();
+	}
+}
