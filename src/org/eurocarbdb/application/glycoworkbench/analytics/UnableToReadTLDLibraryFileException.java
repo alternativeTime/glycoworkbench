9f7efb452e9d2e8215dc8aff54a864c6ec18f0e7
David Damerell (david <David Damerell (david@nixbioinf.org)>
2011-11-23 17:22:15 +0000

    Quick script to analyse Apache logs

---
+++ src/org/eurocarbdb/application/glycoworkbench/analytics/UnableToReadTLDLibraryFileException.java
@@ -0,0 +1,29 @@
+package org.eurocarbdb.application.glycoworkbench.analytics;
+
+public class UnableToReadTLDLibraryFileException extends Exception {
+
+	public UnableToReadTLDLibraryFileException() {
+	}
+
+	public UnableToReadTLDLibraryFileException(String message) {
+		super(message);
+
+	}
+
+	public UnableToReadTLDLibraryFileException(Throwable cause) {
+		super(cause);
+
+	}
+
+	public UnableToReadTLDLibraryFileException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public UnableToReadTLDLibraryFileException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+
+	}
+
+}
